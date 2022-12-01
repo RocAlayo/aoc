70c6593b226d3547bdd87331656a4db46af3fe46
Roc <roc@rocalayo.com>
2022-12-01 18:52:36 +0000

    More consice but harder to read exercice 2 day 1

---
+++ 2022/day-1/bonus.exs
@@ -0,0 +1,19 @@
+File.read!("./full.txt")
+  |> String.split("\n")
+  |> Enum.chunk_by(&(&1 == ""))
+  |> Enum.filter(
+      fn array ->
+        Enum.all?(array, &(String.length(&1) > 0))
+      end
+   )
+  |> Enum.map(
+      fn array ->
+        array
+          |> Enum.map(&String.to_integer/1)
+          |> Enum.reduce(0, &(&1 + &2))
+      end
+    )
+  |> Enum.sort(&(&1 >= &2))
+  |> Enum.slice(0..2)
+  |> Enum.reduce(0, &(&1 + &2))
+  |> IO.inspect
