6eeeab28944ab046caf9430a7edd0f3ea8deee75
Roc <roc@rocalayo.com>
2022-12-06 09:42:39 +0000

    day 6

--- 2022/day-5/part2.exs
+++ 2022/day-5/part2.exs
@@ -27,11 +27,6 @@ directions = directionsRaw
   end)
 
 defmodule Recursion do
-  def print_multiple_times(msg, n) when n > 0 do
-    IO.puts(msg)
-    print_multiple_times(msg, n - 1)
-  end
-
   def tranformStorage(directions, crates) when length(directions) > 0 do
     IO.inspect(directions)
     IO.inspect(crates)
---
+++ 2022/day-6/part1.exs
@@ -0,0 +1,20 @@
+chars = "./full.txt"
+  |> File.read!()
+
+chars
+  |> String.graphemes
+  |> Enum.reduce({[], 0}, fn
+    (_, {array, index}) when is_number(array) -> {array, index}
+    (_, {array, index}) when length(array) == 4 -> {index, index}
+    (char, {array, index}) ->
+      if !(char in array) do
+        {array ++ [char], index + 1}
+      else
+        newArray = array ++ [char]
+          |> Enum.drop_while(&(&1 != char))
+          |> List.delete_at(0)
+        {newArray, index + 1}
+      end
+  end)
+  |> elem(0)
+  |> IO.inspect
