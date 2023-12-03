9d1fee58fbedd21689ebcd818c1801871aafae2c
Roc Alayo <ralayo@userzoom.com>
2023-12-03 20:56:51 +0100

    feat: exercice 2

---
+++ 2023/day-2/ex1/ex1.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func open_file(path string) *os.File {
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return file
+}
+
+func color_to_index(color string) int {
+	switch color {
+	case "red":
+		return 0
+	case "green":
+		return 1
+	case "blue":
+		return 2
+	}
+	return -1
+}
+
+func parse_game(line string) [][]int {
+	string_sets := strings.Split(line, ": ")
+	sets := strings.Split(string_sets[1], "; ")
+	var game = make([][]int, len(sets))
+	for index, set := range sets {
+		cubes := strings.Split(set, ", ")
+		game[index] = []int{0, 0, 0}
+		for _, cube := range cubes {
+			parsed_cube := strings.Split(cube, " ")
+			num, err := strconv.Atoi(parsed_cube[0])
+			if err != nil {
+				panic(err)
+			}
+			game[index][color_to_index(parsed_cube[1])] = num
+		}
+	}
+	return game
+}
+
+func is_game_possible(game [][]int) bool {
+	// 12 red cubes, 13 green cubes, and 14 blue cubes
+	max := []int{12, 13, 14}
+	for _, set := range game {
+		for index, number := range set {
+			if number > max[index] {
+				return false
+			}
+		}
+	}
+
+	return true
+}
+
+func parse_game_number(line string) int {
+	string_sets := strings.Split(line, ": ")
+	string_number := strings.Replace(string_sets[0], "Game ", "", 1)
+	number, err := strconv.Atoi(string_number)
+	if err != nil {
+		panic(err)
+	}
+	return number
+}
+
+func main() {
+	file := open_file("./input.txt")
+	scanner := bufio.NewScanner(file)
+	var sum int = 0
+	for scanner.Scan() {
+		line := scanner.Text()
+		var game_number = parse_game_number(line)
+		var game = parse_game(line)
+		if is_game_possible(game) {
+			sum += game_number
+		}
+	}
+	fmt.Println("total:", sum)
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
